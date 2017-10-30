ad2d349dfb47a40740f843af6361300ea978862b
mbateman5 <mbateman5@studentmail.nait.ca>
2017-10-30 14:21:28 -0600

    Created project skeleton

---
+++ eBikesProjectTeamD/eBikesWebsite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eBikesWebsite.Startup))]
+namespace eBikesWebsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
